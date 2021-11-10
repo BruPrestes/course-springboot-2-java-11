3e12c76f7c0ff17cec245df206e26ea446c3543f
BruPrestes <brunnoprestes@gmail.com>
2021-11-10 18:47:09 -0300

    Payment, one-to-one association

--- src/main/java/com/bpotecnologia/course/config/TestConfig.java
+++ src/main/java/com/bpotecnologia/course/config/TestConfig.java
@@ -16,6 +16,7 @@ import com.bpotecnologia.course.config.repositories.UserRepository;
 import com.bpotecnologia.course.entities.Category;
 import com.bpotecnologia.course.entities.Order;
 import com.bpotecnologia.course.entities.OrderItem;
+import com.bpotecnologia.course.entities.Payment;
 import com.bpotecnologia.course.entities.Product;
 import com.bpotecnologia.course.entities.User;
 import com.bpotecnologia.course.entities.enums.OrderStatus;
@@ -81,5 +82,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/bpotecnologia/course/entities/Order.java
+++ src/main/java/com/bpotecnologia/course/entities/Order.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -13,6 +14,7 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 import com.bpotecnologia.course.entities.enums.OrderStatus;
@@ -38,7 +40,10 @@ public class Order implements Serializable {
 	
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
-
+	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 
 	}
@@ -89,6 +94,16 @@ public class Order implements Serializable {
 		return items;
 	}
 	
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
