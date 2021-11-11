7a53a5f393fe451cd96b4b04e6ea549a9f1c5239
BruPrestes <brunnoprestes@gmail.com>
2021-11-10 23:33:24 -0300

    User update

--- src/main/java/com/bpotecnologia/course/services/UserService.java
+++ src/main/java/com/bpotecnologia/course/services/UserService.java
@@ -2,8 +2,6 @@ package com.bpotecnologia.course.services;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.Optional;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -32,5 +30,17 @@ public class UserService {
 	
 	public void delete(Long id) {
 		repository.deleteById(id);
+	}
+	
+	public User update(Long id, User obj) {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(User entity, User obj) {
+		entity.setName(obj.getName());
+		entity.setEmail(obj.getEmail());
+		entity.setPhone(obj.getPhone());
 	}
 }
