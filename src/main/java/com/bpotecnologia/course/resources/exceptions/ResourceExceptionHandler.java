3b6cbd108eef6430aabef3e0e0d5d166c653eb93
BruPrestes <brunnoprestes@gmail.com>
2021-11-13 14:33:23 -0300

    Exception handling - update

--- src/main/java/com/bpotecnologia/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/bpotecnologia/course/resources/exceptions/ResourceExceptionHandler.java
@@ -24,10 +24,12 @@ public class ResourceExceptionHandler {
 	}
 	
 	@ExceptionHandler(DatabaseException.class)
-	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError>
+	database(DatabaseException e, HttpServletRequest request) {
 		String error = "Database error";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(),
+				status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 	
