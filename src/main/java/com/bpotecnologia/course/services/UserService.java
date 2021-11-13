1100342d7b65bb0e1dda54c5ee75a1d75d4ec3e7
BruPrestes <brunnoprestes@gmail.com>
2021-11-13 13:51:58 -0300

    Exception handling - delete

--- src/main/java/com/bpotecnologia/course/services/UserService.java
+++ src/main/java/com/bpotecnologia/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.bpotecnologia.course.config.repositories.UserRepository;
 import com.bpotecnologia.course.entities.User;
+import com.bpotecnologia.course.services.exceptions.DatabaseException;
 import com.bpotecnologia.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -30,7 +33,14 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
 	}
 	
 	public User update(Long id, User obj) {
